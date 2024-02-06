32236f3688ed70cce4a191022963a0e41c8a2716
Leonid Yuriev <leonid.rmail@gmail.com>
2024-02-06 09:13:20 +0400

    feat: package for paywalls test integrated

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["OnboardingiOSSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/onboarding-online/swift-screens-graph", from: "1.0.0"),
+        .package(url: "https://github.com/onboarding-online/swift-screens-graph", branch: "feature/paywalls"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
